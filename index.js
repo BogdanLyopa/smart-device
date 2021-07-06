53d5b2a13b19c961cceaa109399dffd0e8101369
BogdanLyopa <bogdanlyopa@gmail.com>
2021-07-06 13:20:11 +0300

    add modal and custom checkbox

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector(".close-button"),
+  modal: document.querySelector(".backdrop"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+}
