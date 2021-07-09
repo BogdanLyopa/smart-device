8bf926be939fb50e32e6b268e59d2df84acfde93
BogdanLyopa <bogdanlyopa@gmail.com>
2021-07-09 11:30:42 +0300

    changed

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const refs = {
   openModalBtn: document.querySelector("[data-modal-open]"),
-  closeModalBtn: document.querySelector(".close-button"),
+  closeModalBtn: document.querySelector(".button--close"),
   modal: document.querySelector(".backdrop"),
+  showListBtn: document.querySelector("[data-show-list]"),
+  showAddressBtn: document.querySelector("[data-show-address]"),
+  address: document.querySelector(".footer__address"),
+  list: document.querySelector(".footer__list"),
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.showListBtn.addEventListener("click", showList);
+refs.showAddressBtn.addEventListener("click", showAddress);
 
 function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
+  refs.modal.classList.toggle("backdrop__hidden");
+}
+
+function showList() {
+  refs.showListBtn.classList.toggle("button--plus");
+  refs.showListBtn.classList.toggle("button--minus");
+  refs.list.classList.toggle("visually-hidden");
+}
+
+function showAddress() {
+  refs.showAddressBtn.classList.toggle("button--minus");
+  refs.showAddressBtn.classList.toggle("button--plus");
+  refs.address.classList.toggle("visually-hidden");
 }
